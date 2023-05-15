62896830c9e1b06b171a6d352bf8b547be84eefb
梦境迷离 <dreamylost@outlook.com>
2023-05-15 21:04:06 +0800

    optimize code

--- ziogrpc-v06x-plugin/src/main/scala/org/bitlap/skywalking/apm/plugin/ziogrpc/v06x/call/ZioGrpcServerSendMessageInterceptor.scala
+++ ziogrpc-v06x-plugin/src/main/scala/org/bitlap/skywalking/apm/plugin/ziogrpc/v06x/call/ZioGrpcServerSendMessageInterceptor.scala
@@ -15,7 +15,7 @@ import org.apache.skywalking.apm.agent.core.plugin.interceptor.enhance.*
 import org.apache.skywalking.apm.agent.core.plugin.interceptor.enhance.InstanceMethodsAroundInterceptor
 import org.apache.skywalking.apm.network.trace.component.ComponentsDefine
 import org.bitlap.skywalking.apm.plugin.common.*
-import org.bitlap.skywalking.apm.plugin.ziogrpc.v06x.{ InterceptorSendMessageThreadContext, InterceptorThreadContext }
+import org.bitlap.skywalking.apm.plugin.ziogrpc.v06x.*
 
 /** @author
  *    梦境迷离
