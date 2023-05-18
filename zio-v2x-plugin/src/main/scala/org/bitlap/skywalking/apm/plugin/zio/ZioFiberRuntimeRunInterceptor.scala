df1cf04fd92782bfa6d895f96ae0f519b04e4bed
梦境迷离 <dreamylost@outlook.com>
2023-05-18 17:44:28 +0800

    fix

--- zio-v2x-plugin/src/main/scala/org/bitlap/skywalking/apm/plugin/zio/ZioFiberRuntimeRunInterceptor.scala
+++ zio-v2x-plugin/src/main/scala/org/bitlap/skywalking/apm/plugin/zio/ZioFiberRuntimeRunInterceptor.scala
@@ -24,7 +24,7 @@ final class ZioFiberRuntimeRunInterceptor extends InstanceMethodsAroundIntercept
     argumentsTypes: Array[Class[?]],
     result: MethodInterceptResult
   ): Unit =
-    if objInst == null || !objInst.isInstanceOf[FiberRuntime[?, ?]] then {
+    if objInst == null then {
       return
     }
 
