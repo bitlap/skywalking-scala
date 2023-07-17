dce811d4b3bcbe24b84ac6b83b300005b4e654f4
梦境迷离 <dreamylost@outlook.com>
2023-07-17 17:12:35 +0800

    rename shell

--- packageJars-v1.sh
+++ packageJars-v1.sh
@@ -1,22 +1,21 @@
 #!/bin/bash
 
-folder=dist-v1
-
+folder=dist-v2
 rm -rf $folder
 mkdir $folder
 
 version=`cat version.sbt | awk -F '"' '{print $2}'`
 ziohttpv2=ziohttp-v2x-plugin
-ziogrpcrc=ziogrpc-v06rcx-plugin
+ziogrpctest=ziogrpc-v06testx-plugin
 caliban=caliban-v2x-plugin
-zio203=zio-v203-plugin
+zio200=zio-v200-plugin
 catseffect3=cats-effect-v3x-plugin
 
 
 sbt clean assembly
 
-cp plugins/$zio203/target/apm-$zio203-$version.jar $folder/apm-$zio203-$version.jar
+cp plugins/$zio200/target/apm-$zio200-$version.jar $folder/apm-$zio200-$version.jar
 cp plugins/$ziohttpv2/target/apm-$ziohttpv2-$version.jar $folder/apm-$ziohttpv2-$version.jar
-cp plugins/$ziogrpcrc/target/apm-$ziogrpcrc-$version.jar $folder/apm-$ziogrpcrc-$version.jar
+cp plugins/$ziogrpctest/target/apm-$ziogrpctest-$version.jar $folder/apm-$ziogrpctest-$version.jar
 cp plugins/$caliban/target/apm-$caliban-$version.jar $folder/apm-$caliban-$version.jar
 cp plugins/$catseffect3/target/apm-$catseffect3-$version.jar $folder/apm-$catseffect3-$version.jar
--- packageJars-v2.sh
+++ packageJars-v2.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
 
-folder=dist-v2
+folder=dist-v1
+
 rm -rf $folder
 mkdir $folder
 
 version=`cat version.sbt | awk -F '"' '{print $2}'`
 ziohttpv2=ziohttp-v2x-plugin
-ziogrpctest=ziogrpc-v06testx-plugin
+ziogrpcrc=ziogrpc-v06rcx-plugin
 caliban=caliban-v2x-plugin
-zio200=zio-v200-plugin
+zio203=zio-v203-plugin
 catseffect3=cats-effect-v3x-plugin
 
 
 sbt clean assembly
 
-cp plugins/$zio200/target/apm-$zio200-$version.jar $folder/apm-$zio200-$version.jar
+cp plugins/$zio203/target/apm-$zio203-$version.jar $folder/apm-$zio203-$version.jar
 cp plugins/$ziohttpv2/target/apm-$ziohttpv2-$version.jar $folder/apm-$ziohttpv2-$version.jar
-cp plugins/$ziogrpctest/target/apm-$ziogrpctest-$version.jar $folder/apm-$ziogrpctest-$version.jar
+cp plugins/$ziogrpcrc/target/apm-$ziogrpcrc-$version.jar $folder/apm-$ziogrpcrc-$version.jar
 cp plugins/$caliban/target/apm-$caliban-$version.jar $folder/apm-$caliban-$version.jar
 cp plugins/$catseffect3/target/apm-$catseffect3-$version.jar $folder/apm-$catseffect3-$version.jar
