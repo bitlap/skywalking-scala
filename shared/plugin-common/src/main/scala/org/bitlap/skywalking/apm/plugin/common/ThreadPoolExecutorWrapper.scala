68891149ecd2dd89e5b75104dcb7de9f50122c8f
梦境迷离 <dreamylost@outlook.com>
2024-01-01 19:38:58 +0800

    fix

--- shared/plugin-common/src/main/scala/org/bitlap/skywalking/apm/plugin/common/ThreadPoolExecutorWrapper.scala
+++ shared/plugin-common/src/main/scala/org/bitlap/skywalking/apm/plugin/common/ThreadPoolExecutorWrapper.scala
@@ -3,7 +3,8 @@ package org.bitlap.skywalking.apm.plugin.common
 import java.util
 import java.util.concurrent.*
 
-import org.apache.skywalking.apm.agent.core.context.ContextManager
+import org.apache.skywalking.apm.agent.core.context.{ ContextManager, ContextSnapshot }
+import org.apache.skywalking.apm.agent.core.plugin.interceptor.enhance.EnhancedInstance
 import org.bitlap.skywalking.apm.plugin.common.*
 
 final class ThreadPoolExecutorWrapper(threadPoolExecutor: ThreadPoolExecutor)
@@ -17,51 +18,13 @@ final class ThreadPoolExecutorWrapper(threadPoolExecutor: ThreadPoolExecutor)
       threadPoolExecutor.getRejectedExecutionHandler
     ) {
 
-  override def shutdown(): Unit = threadPoolExecutor.shutdown()
-
-  override def shutdownNow(): util.List[Runnable] = threadPoolExecutor.shutdownNow()
-
-  override def isShutdown: Boolean = threadPoolExecutor.isShutdown
-
-  override def isTerminated: Boolean = threadPoolExecutor.isTerminated
-
-  override def awaitTermination(timeout: Long, unit: TimeUnit): Boolean =
-    threadPoolExecutor.awaitTermination(timeout, unit)
-
-  override def submit[T](task: Callable[T]): Future[T] =
-    threadPoolExecutor.submit(
-      new CallableWrapper[T](task, ContextManager.capture(), threadPoolExecutor.getClass.getName, "submit")
-    )
-
-  override def submit[T](task: Runnable, result: T): Future[T] =
-    threadPoolExecutor.submit(
-      new RunnableWrapper(task, ContextManager.capture(), threadPoolExecutor.getClass.getName, "submit"),
-      result
-    )
-
-  override def submit(task: Runnable): Future[?] =
-    threadPoolExecutor.submit(
-      new RunnableWrapper(task, ContextManager.capture(), threadPoolExecutor.getClass.getName, "submit")
-    )
-
-  override def invokeAll[T](tasks: util.Collection[? <: Callable[T]]): util.List[Future[T]] =
-    threadPoolExecutor.invokeAll(tasks)
-
-  override def invokeAll[T](
-    tasks: util.Collection[? <: Callable[T]],
-    timeout: Long,
-    unit: TimeUnit
-  ): util.List[Future[T]] =
-    threadPoolExecutor.invokeAll(tasks, timeout, unit)
-
-  override def invokeAny[T](tasks: util.Collection[? <: Callable[T]]): T =
-    threadPoolExecutor.invokeAny(tasks)
-
-  override def invokeAny[T](tasks: util.Collection[? <: Callable[T]], timeout: Long, unit: TimeUnit): T =
-    threadPoolExecutor.invokeAny(tasks, timeout, unit)
-
   override def execute(command: Runnable): Unit =
-    threadPoolExecutor.execute(
-      new RunnableWrapper(command, ContextManager.capture(), threadPoolExecutor.getClass.getName, "execute")
-    )
+    command match
+      case e: EnhancedInstance
+          if e.getSkyWalkingDynamicField.isInstanceOf[ContextSnapshot] || !ContextManager.isActive =>
+        threadPoolExecutor.execute(command)
+      case _ =>
+        threadPoolExecutor.execute(
+          new RunnableWrapper(command, ContextManager.capture(), threadPoolExecutor.getClass.getName, "execute")
+        )
 }
